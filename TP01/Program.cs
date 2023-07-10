b15923b00f3e16ebc13616c275327b3080b471bb
Tahiel24 <tahielvarela@gmail.com>
2023-07-10 13:01:22 -0300

    Push en rama nueva

--- TP01/Program.cs
+++ TP01/Program.cs
@@ -1,5 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Muy buenas, mundo");
-Console.Write("Ingrese un numero: ");
-double num= Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Cuadrado del numero: "+ Convert.ToDouble(Math.Pow(num,2)));  
+﻿Console.WriteLine("Probando una nueva rama");
