3a66dd34c56dc87ee28d51d82ff69982501e3035
Tahiel24 <tahielvarela@gmail.com>
2023-07-10 11:41:20 -0300

    Primer commit

---
+++ TP01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Muy buenas, mundo");
+Console.Write("Ingrese un numero: ");
+double num= Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Cuadrado del numero: "+ Convert.ToDouble(Math.Pow(num,2)));  
